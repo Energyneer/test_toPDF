4443ea10e5318f8e9e4e3d1231916d1b678d1575
Konstantin Energyneer <ennergyneer@gmail.com>
2023-01-04 23:03:13 +0400

    v0.003 Added nuget packages

--- Core/Class1.cs
+++ Core/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Core
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Core
 {
     public class Class1
     {
-
+        public static void testc()
+        {
+            var doc = new Document();
+        }
     }
 }
