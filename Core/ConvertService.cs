488c32c505ad4e70840cc4e9f311f970a272d2cf
Konstantin Energyneer <ennergyneer@gmail.com>
2023-01-05 21:26:26 +0400

    v0.004 Test create document

---
+++ Core/ConvertService.cs
@@ -0,0 +1,37 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Fonts;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class ConvertService
+    {
+        public void Convert()
+        {
+
+            GlobalFontSettings.FontResolver = new FontResolver();
+
+            var document = new PdfDocument();
+            var page = document.AddPage();
+
+            var gfx = XGraphics.FromPdfPage(page);
+            var font = new XFont("Arial", 20, XFontStyle.Bold);
+
+            var textColor = XBrushes.Black;
+            var layout = new XRect(20, 20, page.Width, page.Height);
+            var format = XStringFormats.Center;
+
+
+            gfx.DrawString("Hello World!", font, textColor, layout, format);
+
+            document.Save(@"C:\Users\enner\Downloads\helloworld.pdf");
+        }
+    }
+}
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample;
+
+public class Program
+{
+    public static void Main()
+    {
+        var service = new ConvertService();
+        service.Convert();
+
+    }
+}
